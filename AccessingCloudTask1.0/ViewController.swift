dffcff67862e80084630973d151be57d7bda7675
Luis Perez Romero <pinoluis02@gmail.com>
2015-12-12 18:47:17 -0600

    first commit, all points cover

--- AccessingCloudTask1.0/ViewController.swift
+++ AccessingCloudTask1.0/ViewController.swift
@@ -8,18 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    let URL_String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+    @IBOutlet weak var textFieldISBN: UITextField!
+    @IBOutlet weak var textViewResult: UITextView!
+    @IBOutlet weak var buttonClear: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textFieldISBN.delegate = self
+        textFieldISBN.returnKeyType = .Search
+        
+        textViewResult.layer.cornerRadius = 10
+        buttonClear.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func clearISBN(sender: AnyObject) {
+        textViewResult.text = ""
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if let inputISBN = textFieldISBN.text {
+            requestISBN(inputISBN)
+        }
+        
+        return true
+    }
+    
+    func requestISBN(ISBN: String) {
+        let urlRequest = URL_String + ISBN
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlRequest)
+        
+        let task = session.dataTaskWithURL(url!) {
+            (data, response, error) -> Void in
+            
+            if error != nil {
+                print(error!.localizedDescription)
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.showErrorAlert(error!.localizedDescription)
+                })
+            }
+            else {
+                if let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding) {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.textViewResult.text = String(dataString)
+                    })
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func showErrorAlert(error: String) {
+        let alert = UIAlertController(title: "Network Error", message: error, preferredStyle: .Alert)
+        let dismiss = UIAlertAction(title: "Dismiss", style: .Cancel, handler: nil)
+        alert.addAction(dismiss)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
